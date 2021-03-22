85b83376da35abbce10c251a222543ae90212f13
maka31 <makhburat.asanova@gmail.com>
2021-03-22 15:29:28 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,9 @@
+public abstract class Hero implements HavingSuperAbility {
+    int health;
+    int damage;
+    String superAbilityType;
+
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+    }
+}
---
+++ src/Medic.java
@@ -0,0 +1,7 @@
+public class Medic extends Hero {
+
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        System.out.println("Medic применил суперспособность " + superAbilityType);
+    }
+}
